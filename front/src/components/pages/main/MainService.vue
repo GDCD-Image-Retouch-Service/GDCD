a7486bf0fc3c1d50b4f72c691dfc2d2dbda41bc2
JunHyuk Park <73526186+inte9ral@users.noreply.github.com>
2022-09-15 10:43:25 +0900

    Revert "[S07P22B301-95] Fix. 레이아웃:캠연결 테스트"
    This reverts commit e16ca29c8cdd5331e367282370ec1ba1ca61a068.

--- front/src/components/pages/main/MainService.vue
+++ front/src/components/pages/main/MainService.vue
@@ -2,46 +2,19 @@
   <div class="main-service">
     <input type="file" accept="image/*;capture=camera" />
     디자인 공사중 입니다
-    <toggle-button class="width:100%" />
-    <cam-container />
+    <ToggleButton class="width:100%" />
     공사중 이ㅣㅂ니다
-    <image-input />
-    <score-receive-button />
-    <how-to-use />
+    <ImageInput />
+    <ScoreReceiveButton />
+    <HowToUse />
   </div>
 </template>
 
 <script setup>
-import CamContainer from '@/components/molecules/CamContainer.vue';
 import ToggleButton from '@/components/molecules/ToggleButton.vue';
 import ImageInput from '@/components/organisms/ImageInput.vue';
 import ScoreReceiveButton from '@/components/atoms/button/ScoreReceiveButton.vue';
 import HowToUse from '@/components/molecules/HowToUse.vue';
-
-var video = document.querySelector('#videoElement');
-
-if (navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices
-    .getUserMedia({ video: true })
-    .then(function (stream) {
-      video.srcObject = stream;
-    })
-    .catch(function (e) {
-      console.log('Something went wrong!');
-    });
-}
-
-function stop(e) {
-  var stream = video.srcObject;
-  var tracks = stream.getTracks();
-
-  for (var i = 0; i < tracks.length; i++) {
-    var track = tracks[i];
-    track.stop();
-  }
-
-  video.srcObject = null;
-}
 </script>
 
 <style>
