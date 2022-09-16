ab3e6b9a70c9a776fe8a265f77781104bb5c3d6e
홍성덕 <tjdejr338@naver.com>
2022-09-16 17:49:56 +0900

    [S07P22B301-106] Feat. 게시글 디테일: 채팅창 임시구현

--- front/src/components/pages/community/CommunityDetailChatting.vue
+++ front/src/components/pages/community/CommunityDetailChatting.vue
@@ -1,26 +1,14 @@
 <template>
   <div class="community-detail-chatting">
-    <div>
-      <div v-for="ch in chat" :key="ch.id" class="chatting-wrap">
-        <!-- 유저 정보 -->
-        <div class="user-info">
-          <img :src="ch.profileImage" class="profile-image" />
-
-          <div>
-            {{ ch.username }}
-          </div>
-        </div>
-
-        <!-- 메시지 -->
-        <div>
-          {{ ch.message }}
-        </div>
-      </div>
-    </div>
+    <community-detail-chatting-item :chat="chat" />
+    <community-detail-chatting-input :chat="chat[0]" />
   </div>
 </template>
 
 <script setup>
+import CommunityDetailChattingItem from '@/components/organisms/CommunityDetailCahttingItem.vue';
+import CommunityDetailChattingInput from '@/components/organisms/CommunityDetailChattingInput.vue';
+
 const chat = [
   {
     id: 1,
@@ -42,23 +30,5 @@ const chat = [
 <style scoped>
 .community-detail-chatting {
   width: calc(100% - 40px);
-}
-.chatting-wrap {
-  align-items: center;
-  display: flex;
-  /* justify-content: space-between; */
-  gap: 10px;
-  margin-bottom: 20px;
-  width: 100%;
-}
-.user-info {
-  display: flex;
-  align-items: center;
-}
-.profile-image {
-  width: 50px;
-  height: 50px;
-  object-fit: cover;
-  border-radius: 50px;
 }
 </style>
