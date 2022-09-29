7bc6d4800a531821a85ff4e65cc97a9756ae0799
JunHyuk Park <73526186+inte9ral@users.noreply.github.com>
2022-09-29 13:01:20 +0900

    [S07P22B301-325] feat. 2차점수반환 : 중간저장

--- front/src/components/organisms/main/OptimizeCard.vue
+++ front/src/components/organisms/main/OptimizeCard.vue
@@ -2,15 +2,111 @@
   <div
     class="optimize-card outer d-flex flex-column align-items-center justify-content-center"
   >
-    <b-carousel controls indicators>
-      <b-carousel-slide
-        active
-        img-src="https://picsum.photos/1024/480/?image=30"
+    <div
+      id="carouselExampleCaptions"
+      class="carousel slide"
+      :class="[$root.theme ? '' : 'carousel-dark']"
+      data-bs-ride="false"
+      style="width: 100%"
+    >
+      <!-- carousel bottom button -->
+      <div class="carousel-indicators">
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="0"
+          class="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+      </div>
+
+      <!-- carousel contents -->
+      <div class="carousel-inner" style="height: 100%">
+        <div class="carousel-item active" style="height: 100%">
+          <div class="carousel-item-1 itemBox d-flex" style="height: 100%">
+            <div
+              class="d-flex flex-column align-items-center justify-content-center"
+              style="height: 100%; width: 400px"
+            >
+              <div class="textBox">
+                <img src="https://picsum.photos/200/300" alt="임시" />
+
+                <h2>사진</h2>
+                <div class="spacer"></div>
+                <p>점수</p>
+              </div>
+              <img
+                src="@/assets/grade/1.png"
+                style="height: 100px; object-fit: cover"
+                alt="logo"
+              />
+            </div>
+            <div class="carousel-item-1-img flex-grow-1"></div>
+          </div>
+        </div>
+
+        <div class="carousel-item" style="height: 100%">
+          <div class="carousel-item-2 itemBox d-flex" style="height: 100%">
+            <div class="container-fluid" style="height: 100%; width: 100%">
+              <div class="row" style="height: 100%; width: 100%">
+                <div
+                  class="textBox col-12 col-sm-4"
+                  style="padding-top: 24vh; padding-left: 8%"
+                >
+                  <h2>
+                    <span>사진</span>
+                  </h2>
+                  <div class="spacer"></div>
+                  <p>optimize 점수 내역:</p>
+                  <p>tag</p>
+                </div>
+
+                <div
+                  class="carousel-item-2-img col-12 col-sm-8"
+                  :style="{
+                    'min-height': '50%',
+                    'background-image':
+                      'url(' + require('@/assets/grade/1.png') + ')',
+                    'background-size': 'contain',
+                    'background-repeat': 'no-repeat',
+                    'background-position': 'center',
+                  }"
+                ></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- carousel side button -->
+      <button
+        class="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="prev"
+        style="width: 4%"
       >
-        <h1>First</h1>
-        <p>Some</p>
-      </b-carousel-slide>
-    </b-carousel>
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button
+        class="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="next"
+        style="width: 4%"
+      >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
   </div>
 </template>
 
