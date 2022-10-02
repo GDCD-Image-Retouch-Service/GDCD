a13138cc862c23b3b59cfa11a9badb659207db4f
Aeri <96863180+dofl8787@users.noreply.github.com>
2022-10-03 02:04:44 +0900

    [S07P22B301-291] fix. 커뮤니티 : 게시글, 커맨트, 좋아요, 스크랩 리스트 역순 수정

--- back/src/main/java/com/gdcd/back/domain/comment/CommentRepository.java
+++ back/src/main/java/com/gdcd/back/domain/comment/CommentRepository.java
@@ -7,6 +7,12 @@ import java.util.List;
 
 public interface CommentRepository extends MongoRepository<Comment, Long> {
     List<Comment> findAllByWriter(UserSimple writer);
+
+    List<Comment> findAllByWriterOrderByRegistDateDesc(UserSimple writer);
     List<Comment> findAllByPostIdAndUpperAndValidation(Long postId, Long upper, boolean validation);
+    List<Comment> findAllByPostIdAndUpperAndValidationOrderByRegistDateDesc(Long postId, Long upper, boolean validation);
+
     List<Comment> findAllByUpperAndValidation(Long upper, boolean validation);
+    List<Comment> findAllByUpperAndValidationOrderByRegistDateDesc(Long upper, boolean validation);
+
 }
