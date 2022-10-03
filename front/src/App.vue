20da16c9274080b6a9c202fcc7f5de6250b4ffeb
홍성덕 <tjdejr338@naver.com>
2022-10-03 14:18:20 +0900

    [S07P22B301-316] Fix. 병합: 머지 과정에서 충돌된 부분 fix

--- front/src/App.vue
+++ front/src/App.vue
@@ -16,7 +16,10 @@ import { computed, ref, onBeforeMount } from 'vue';
 
 import { useUserStore } from '@/stores';
 
+import { computed } from 'vue';
 import { useMainStore } from '@/stores';
+
+
 
 const mainStore = useMainStore();
 
@@ -27,7 +30,7 @@ if (localStorage.getItem('token')) {
   userStore.getMyinfo();
 }
 
-const theme = computed(() => (mainStore.getIsDark ? 'dark' : 'light'));
+const theme = computed(() => (homeStore.getIsDark ? 'dark' : 'light'));
 
 onBeforeMount(() => {
   window.addEventListener('scroll', handleScroll);
@@ -58,6 +61,7 @@ document.addEventListener('scroll', (e) => {
     isVisible.value = false;
   }
 });
+
 </script>
 
 <style>
